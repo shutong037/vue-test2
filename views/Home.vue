eaeb23243ec485f9dbe2c0415699dcd43df69857
书童 <25699434+shutong037@users.noreply.github.com>
2021-09-01 14:44:17 +0800

    Add files via upload

---
+++ views/Home.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <p>开始进行学习 {{num}}</p>
+  </div>
+</template>
+
+<script>
+import { formatDate } from '@/utils/common.js'
+
+export default {
+  name: 'Home',
+  data () {
+    return {
+      num: '456'
+    }
+  },
+  mounted () {
+    this.init()
+  },
+  methods: {
+    init () {
+      const time = formatDate(new Date())
+      console.log(time)
+    }
+  }
+}
+</script>
