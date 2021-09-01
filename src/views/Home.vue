a2d4c32baa1b2517f482dd822197e091f1329355
书童 <25699434+shutong037@users.noreply.github.com>
2021-09-01 13:46:32 +0800

    Add files via upload

---
+++ src/views/Home.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <p>开始进行学习 {{num}}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+  data () {
+    return {
+      num: '456'
+    }
+  },
+  mounted () {
+    this.init()
+  },
+  methods: {
+    init () {
+      console.log('123')
+    }
+  }
+}
+</script>
