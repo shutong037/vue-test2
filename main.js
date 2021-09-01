7be809c1a57932e994379ff955c3be650298d765
书童 <25699434+shutong037@users.noreply.github.com>
2021-09-01 14:12:31 +0800

    Add files via upload
    添加页面标题

---
+++ main.js
@@ -0,0 +1,20 @@
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router'
+import store from './store'
+
+Vue.config.productionTip = false
+
+router.beforeEach((to, from, next) => {
+  /* 路由发生变化修改页面title */
+  if (to.meta.title) {
+    document.title = to.meta.title
+  }
+  next()
+})
+
+new Vue({
+  router,
+  store,
+  render: h => h(App)
+}).$mount('#app')
